e92f53756b539169fa3bbcb98b04a1182ce6aa5b
stevenliao <Stvnliao@yahoo.com>
2017-12-07 15:04:00 -0500

    Move index.js to src and refactor index.js

---
+++ src/index.js
@@ -0,0 +1,70 @@
+const client = require('./client');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter command: ',
+});
+
+const prompt = () => {
+  rl.prompt();
+};
+
+const printHelp = () => {
+  console.log(`
+  Crypto Moneybag
+  ---------------
+  Commands available:
+    notifications
+    buyPrice
+  `);
+};
+
+const parseLine = (line, callback) => {
+  switch (line) {
+    case 'notifications': {
+      client.getNotifications({}, (err, notifications) => {
+        console.log(notifications);
+        callback();
+      });
+      break;
+    }
+
+    case 'buyPrice': {
+      client.getBuyPrice(
+        {
+          currencyPair: 'BTC-USD',
+        },
+        (err, result) => {
+          console.log(result);
+          callback();
+        },
+      );
+      break;
+    }
+
+    case 'exit':
+    case 'quit': {
+      rl.close();
+      break;
+    }
+
+    default:
+      console.log(line);
+      printHelp();
+      callback();
+  }
+};
+
+const handleLine = (line) => {
+  parseLine(line, prompt);
+};
+
+const handleClose = () => {
+  console.log('bye');
+};
+
+printHelp();
+rl.prompt();
+rl.on('line', handleLine).on('close', handleClose);
